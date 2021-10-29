324ce89f148ffd7b053ac23fdacb4a4ce89866e6
Quinten Onya <s113588@ap.be>
2021-10-29 07:10:07 +0200

    first commit

---
+++ Assets/Helicopter/Scripts/UIGameController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIGameController : MonoBehaviour
+{
+    public Text EngineForceView;
+
+
+	// Use this for initialization
+    public static UIGameController runtime;
+
+    private void Awake()
+    {
+        runtime = this;
+        PlayerPrefs.SetInt("Score", 0);
+        EngineForceView.text = "Score : " + PlayerPrefs.GetInt("Score");
+    }
+
+   
+    
+    public void ScoreManager(int score) {
+
+        PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score")+score);
+        EngineForceView.text = "Score : " + PlayerPrefs.GetInt("Score");
+
+    }
+
+}
